482d058b8aede9a7a90bc704963f8091432a972f
paiizz <wanchanapon.t@ku.th>
2017-01-20 16:08:05 +0700

    done code in GuessingGame

---
+++ src/GameConsole.java
@@ -0,0 +1,5 @@
+/**
+ * Created by PaiizZ on 1/20/2017 AD.
+ */
+public class GameConsole {
+}
---
+++ src/GuessingGame.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+/**
+ * Game of guessing a secret number.
+ *
+ * @author Wanchanapon Thanwaranurak
+ *
+ * Created on 1/20/2017.
+ */
+
+public class GuessingGame {
+
+    private int upperBound;
+    private int secret;
+    private String hint;
+
+    GuessingGame(int upperBound){
+        this.upperBound = upperBound;
+        this.secret = getRandomNumber(30) ;
+        this.hint = "I'm thinking of a number between 1 and 30";
+    }
+
+    private int getRandomNumber(int limit){
+        long seed = System.currentTimeMillis();
+        Random rand = new Random(seed);
+        return rand.nextInt(limit)+1 ;
+    }
+
+    public boolean guess (int number){
+        if(this.secret == number){
+            this.setHint("Correct. The secret is "+this.secret+".");
+            return true;
+        } else if (this.secret > number){
+            this.setHint("Sorry, your guess is too small.");
+            return false;
+        } else {
+            this.setHint("Sorry, your guess is too large.");
+            return false;
+        }
+    }
+
+    public void setHint(String hint){
+        this.hint = hint ;
+    }
+
+    public String getHint(){
+        return this.hint;
+    }
+
+
+}
